e6c695a054dc8e60c4d59b289689875c70636074
Paweł Kowalski <pawel.kow.92+github@gmail.com>
2022-10-03 13:21:01 +0200

    Provide wrapper component for managing crawler clients

---
+++ src/main/java/com/github/pawelkowalski92/crawler/configuration/CrawlerClientsConfiguration.java
@@ -0,0 +1,29 @@
+package com.github.pawelkowalski92.crawler.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+import java.time.Duration;
+import java.util.List;
+
+@ConfigurationProperties(prefix = "web-crawlers")
+@ConstructorBinding
+public record CrawlerClientsConfiguration(List<ClientDefinition> definitions) {
+
+    public record ClientDefinition(
+            String tag,
+            boolean primary,
+            Duration connectionTimeout,
+            Duration readTimeout,
+            RetryStrategy retry
+    ) {
+
+    }
+
+    public record RetryStrategy(
+            int maxAttempts,
+            Duration backOff
+    ) {
+
+    }
+}
