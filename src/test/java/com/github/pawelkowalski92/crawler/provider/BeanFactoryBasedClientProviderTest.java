d0d3f5cca4371cccb9f1eebd20d4773a65abba24
Paweł Kowalski <pawel.kow.92+github@gmail.com>
2022-10-03 20:15:56 +0200

    Add ClientProvider test

---
+++ src/test/java/com/github/pawelkowalski92/crawler/provider/BeanFactoryBasedClientProviderTest.java
@@ -0,0 +1,58 @@
+package com.github.pawelkowalski92.crawler.provider;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class BeanFactoryBasedClientProviderTest {
+
+    @Autowired
+    BeanFactoryBasedClientProvider clientProvider;
+
+    @Autowired
+    WebClient defaultClient;
+
+    @ParameterizedTest
+    @ValueSource(strings = {"try-hard", "balanced", "fail-fast"})
+    public void shouldReturnWebClientForGivenTag(String tag) {
+        // given tag
+
+        // when
+        Optional<WebClient> clientOptional = clientProvider.findClient(tag);
+
+        // then
+        assertThat(clientOptional).isPresent();
+    }
+
+    @Test
+    public void shouldReturnPrimaryClientAsDefault() {
+        // given no tag
+
+        // when
+        WebClient client = clientProvider.getDefaultClient();
+
+        // then
+        assertThat(client).isSameAs(defaultClient);
+    }
+
+    @Test
+    public void shouldReturnEmptyForUnknownTag() {
+        // given
+        String tag = "unknown";
+
+        // when
+        Optional<WebClient> clientOptional = clientProvider.findClient(tag);
+
+        // then
+        assertThat(clientOptional).isEmpty();
+    }
+
+}
